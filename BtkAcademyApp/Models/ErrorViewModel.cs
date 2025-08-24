22f835580b5d09ccdc6b12079653b1b972f0a522
Talha Pakdil <talhapakdil7@gmail.com>
2025-08-24 12:25:59 +0300

    project begining

---
+++ BtkAcademyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BtkAcademyApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
