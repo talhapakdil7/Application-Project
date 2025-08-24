a90047c189cd84796c7cb087a46864435d3b191e
Talha Pakdil <talhapakdil7@gmail.com>
2025-08-24 17:41:10 +0300

    course controller and view

---
+++ BtkAcademyApp/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+namespace BtkAcademyApp.Controllers
+{
+
+    public class CourseController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+
+            return View();
+
+        }
+      
+       public IActionResult Apply()
+        {
+
+            return View();
+
+        }
+      
+
+
+    }
+
+
+
+
+}
